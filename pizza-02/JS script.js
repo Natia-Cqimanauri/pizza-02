406f4f060c43027ee7c676c151bbcc6823b140c7
Natia-Cqimanauri <132232010+Natia-Cqimanauri@users.noreply.github.com>
2023-06-01 00:47:52 +0400

    Add files via upload

---
+++ pizza-02/JS script.js
@@ -0,0 +1,6 @@
+let navigation=document.getElementById("navBar");
+let burger=document.getElementById("burgerBar");
+
+burger.addEventListener (click,function(){
+    navigation.classList.add(navActive);
+})
